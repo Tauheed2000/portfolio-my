9b29193ef391ebbc93c37c316d39d597fe7cf333
Tauheed2000 <khantauheed8601@gmail.com>
2023-10-30 20:44:52 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="section">
+      <h2>About Me</h2>
+      <p>I am highly motivated BTech CSE student of VIT Chennai, with robust academic background </p>
+        <p>and versatile skill-set in programming and advanced technologies. My hands-on experience in design</p>
+       <p> and development of next generation software in the field of Data Analytics, AI, ML and IoT during </p>
+       <p> internships at  Netmax Technologies, reflects my practical expertise. I have a track record </p>
+       <p> of successful projects, excelling in hackathons and exhibiting strong leadership</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: mohdtauheed.khan2021@vitstudetn.ac.in</p>
+      <p>Phone: 6392454956</p>
+      <p>LinkedIn:https://linkedin.com/in/mohd-tauheed-khan-b6a33a261</p>
+    </div>
+  );
+};
+
+export default Contact;
